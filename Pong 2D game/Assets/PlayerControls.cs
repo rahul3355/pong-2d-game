7a2b3024764a32a5d372fc76600dc21a9025c4de
rahul3355 <53422351+rahul3355@users.noreply.github.com>
2019-12-28 18:40:31 +0530

    Custom(W and S) Controls

--- Pong 2D game/Assets/PlayerControls.cs
+++ Pong 2D game/Assets/PlayerControls.cs
@@ -4,29 +4,50 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-public float speed;
-//public float Yincrement;
-private Rigidbody2D rb;
-//private Vector2 targetPos;
-private Vector2 moveVelocity;
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    [SerializeField]
+    KeyCode MoveUp;
+    [SerializeField]
+    KeyCode MoveDown;
 
 
-    void Start() 
+     void Start() 
     {
-        rb = GetComponent<Rigidbody2D>();
+         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+     // Update is called once per frame
+     void Update()
+     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * speed;
         
-    }
+     }
 
     void FixedUpdate() {
 
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
         
-    }
+   }
+
+    // [SerializeField]
+    // Vector2 v2Force;
+
+    // [SerializeField]
+    // KeyCode keyPositive;
+    // [SerializeField]
+    // KeyCode keyNegative;
+
+    // void FixedUpdate()
+    // {
+    //     if (Input.GetKey(keyPositive))
+    //         GetComponent<Rigidbody2D>().velocity += v2Force;
+
+    //     if (Input.GetKey(keyNegative))
+    //         GetComponent<Rigidbody2D>().velocity -= v2Force;
+    // }
+
 }
