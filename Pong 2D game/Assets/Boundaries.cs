367d7e19afd161ca704f1d5a302b380349d3cf2a
rahul3355 <53422351+rahul3355@users.noreply.github.com>
2019-12-29 17:44:13 +0530

    added player 2

--- Pong 2D game/Assets/Boundaries.cs
+++ Pong 2D game/Assets/Boundaries.cs
@@ -6,7 +6,7 @@ public class Boundaries : MonoBehaviour
 {
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9.25f, 9.25f),Mathf.Clamp(transform.position.y, -2.75f , 2.75f), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9.25f, 9.25f),Mathf.Clamp(transform.position.y, -2.70f , 2.70f), transform.position.z);
         
         
     }
--- Pong 2D game/Assets/PlayerControls.cs
+++ Pong 2D game/Assets/PlayerControls.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     public float speed;
+
+    public KeyCode moveUp;
+    public KeyCode moveDown;
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
 
      void Start() 
     {
-         rb = GetComponent<Rigidbody2D>();
+         //rb = GetComponent<Rigidbody2D>();
     }
 
      // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
+        //Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //moveVelocity = moveInput.normalized * speed;
+
+        if(Input.GetKeyDown(moveUp))
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 3 , 0);
+
+        if(Input.GetKeyDown(moveDown))
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, -3 , 0);
 
     }
 
     
     void FixedUpdate() {
 
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+       // rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
         
    }
 
