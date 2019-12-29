aaf1ab2f1484d1ab29503213c3d24d23c535f6c9
rahul3355 <53422351+rahul3355@users.noreply.github.com>
2019-12-29 22:13:52 +0530

    Added Ball Control

---
+++ Pong 2D game/Assets/BallControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = Random.Range(1,11);
+        if(num <= 5)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(80, 10));
+        }
+        else{
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-80, -10));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
